85382372e2c22ff12f58761e9d88307823f30452
IyadShobaki <iyadshobaki82@gmail.com>
2020-10-16 12:44:08 -0400

    Create Controllers File
    - Added Controllers file and add home controller.
    - Create first Action method, that handle the request from 3rd party.

---
+++ Tulip-API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tulip_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index() // Action to handle the request that is coming from 3rd party
+        {
+            return Ok("Hello World"); // 200OK
+        }
+    }
+}
