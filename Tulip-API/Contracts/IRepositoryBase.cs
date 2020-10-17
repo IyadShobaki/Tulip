3f22f33bd7beacbe19fca45336cccdda06302200
IyadShobaki <iyadshobaki82@gmail.com>
2020-10-16 20:21:38 -0400

    Setup Inventory API
    - Setup Inventory Repository and Service.

---
+++ Tulip-API/Contracts/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tulip_API.Contracts
+{
+    // Generic (Its function relative to whatever class we choose
+    // from the Data folder
+    interface IRepositoryBase<T> where T : class 
+    {
+        Task<IList<T>> FindAll();
+        Task<T> FindById(int id);
+        Task<bool> Create(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> Delete(T entity);
+        Task<bool> Save();  // To commit the changes to the database
+    }
+}
---
+++ Tulip-API/Services/InventoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tulip_API.Contracts;
+using Tulip_API.Data;
+
+namespace Tulip_API.Services
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public InventoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<bool> Create(Inventory entity)
+        {
+            await _db.Inventories.AddAsync(entity);
+            return await Save();
+
+        }
+
+        public async Task<bool> Delete(Inventory entity)
+        {
+            _db.Inventories.Remove(entity);
+            return await Save();
+        }
+
+        public async Task<IList<Inventory>> FindAll()
+        {
+            var inventories = await _db.Inventories.ToListAsync();
+            return inventories;
+        }
+
+        public async Task<Inventory> FindById(int id)
+        {
+            var inventory = await _db.Inventories.FindAsync(id);
+            return inventory;
+        }
+
+        public async Task<bool> Save()
+        {
+            var changes = await _db.SaveChangesAsync();
+
+            return changes > 0;
+        }
+
+        public async Task<bool> Update(Inventory entity)
+        {
+            _db.Inventories.Update(entity);
+            return await Save();
+        }
+    }
+}
