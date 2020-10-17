58fada20b9ca3965b57560ddb53f667a005fc966
IyadShobaki <iyadshobaki82@gmail.com>
2020-10-16 22:33:15 -0400

    Continue Setup Inventory API
    Setup Get Endpoints.

--- Tulip-API/Controllers/InventoriesController.cs
+++ Tulip-API/Controllers/InventoriesController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Tulip_API.Contracts;
+using Tulip_API.DTOs;
 
 namespace Tulip_API.Controllers
 {
@@ -13,14 +15,80 @@ namespace Tulip_API.Controllers
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    //[ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public class InventoriesController : ControllerBase
     {
         private readonly IInventoryRepository _inventoryRepository;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
 
-        public InventoriesController(IInventoryRepository inventoryRepository)
+        public InventoriesController(IInventoryRepository inventoryRepository,
+            ILoggerService logger,
+            IMapper mapper)
         {
             _inventoryRepository = inventoryRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get All Inventories
+        /// </summary>
+        /// <returns>List of Inventory</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetInventories()
+        {
+            try
+            {
+                _logger.LogInfo("Attempted Get All Inventories");
+                var inventories = await _inventoryRepository.FindAll();
+                var response = _mapper.Map<IList<InventoryDTO>>(inventories);
+                _logger.LogInfo("Successfully got all Inventories");
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"{ex.Message} - {ex.InnerException}");
+                //return StatusCode(500, "Something went wrong. Please contact the administrator"); // 500 something went wrong
+                return InternalError($"{ex.Message} - {ex.InnerException}");
+            }
+        }
+        /// <summary>
+        /// Get Inventory by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>An Inventory's record</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetInventory(int id)
+        {
+            try
+            {
+                _logger.LogInfo($"Attempted to get Inventory with id:{id}");
+                var inventory = await _inventoryRepository.FindById(id);
+                if (inventory == null)
+                {
+                    _logger.LogWarn($"Inventory with id:{id} was not found");
+                    return NotFound(); // 404
+                }
+                var response = _mapper.Map<InventoryDTO>(inventory);
+                _logger.LogInfo($"Successfully got Inventory with id:{id}");
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return InternalError($"{ex.Message} - {ex.InnerException}");
+            }
+        }
+
+        private ObjectResult InternalError(string message)
+        {
+            _logger.LogError(message);
+            return StatusCode(500, "Something went wrong. Please contact the administrator");
         }
     }
 }
