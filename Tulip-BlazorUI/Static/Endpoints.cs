c79f7346cbbc739c13ad4744d1814c96820d916b
IyadShobaki <iyadshobaki82@gmail.com>
2020-10-20 07:34:46 -0400

    Setup HTTP Client And Service Repositories

---
+++ Tulip-BlazorUI/Static/Endpoints.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tulip_BlazorUI.Static
+{
+    public static class Endpoints
+    {
+        public static string BaseUrl = "https://localhost:44353/";
+        public static string CategoriesEndpoint = $"{BaseUrl}api/categories/";
+        public static string ProductsEndpoint = $"{BaseUrl}api/products/";
+
+    }
+}
