e6278f12b9469719de421319ef0f260256f31fdf
IyadShobaki <iyadshobaki82@gmail.com>
2020-10-20 23:48:33 -0400

    Adds Category Detail Page
    - Allow admins to view the details of a specific category.

--- Tulip-BlazorUI/Models/Product.cs
+++ Tulip-BlazorUI/Models/Product.cs
@@ -1,6 +1,20 @@
-﻿namespace Tulip_BlazorUI.Models
+﻿using System;
+
+namespace Tulip_BlazorUI.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string ProductImage { get; set; }
+        public decimal RetailPrice { get; set; }
+        public int QuantityInStock { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public bool IsTaxable { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
     }
 }
